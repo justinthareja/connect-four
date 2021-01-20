c9ea2f714c407c0dfdeda13b5959c5bcb83a5d58
Justin Thareja <justin.thareja@gmail.com>
2021-01-20 13:34:42 -0500

    added play event

--- js/board.js
+++ js/board.js
@@ -9,8 +9,22 @@
         render();
     }
 
+    function handlePlay(columnIndex) {
+        connect4.play(columnIndex);
+        render();
+    }
+
     function handleBoardClick(event) {
-        console.log("hi");
+        if (!event.target.matches("[rel*=js-cell]")) return;
+
+        event.preventDefault();
+
+        let parent = event.target.parentElement;
+        let columnIndex = Array.from(parent.children).findIndex(function matchTarget(child){
+            return child == event.target;
+        });
+
+        EVT.emit('play', columnIndex);
     }
 
     // Takes a board array and returns the corresponding HTML string
@@ -20,12 +34,12 @@
         board.forEach(function renderRow(row) {
             html += `<div class="board-row">`;
             
-            row.forEach(function renderCell(cell) {
+            row.forEach(function renderCell(cell, i) {
                 let classNames = "board-cell"
                 if (cell == 1) classNames += " red";
                 if (cell == 2) classNames += " yellow";
 
-                html += `<div class="${classNames}" rel="js-cell"></div>`;
+                html += `<div class="${classNames}" rel="js-cell-${i}"></div>`;
             });
 
             html += `</div>`;
@@ -39,6 +53,7 @@
     }
 
     EVT.on("init", init);
+    EVT.on("play", handlePlay);
     
     var publicAPI = {};
 
