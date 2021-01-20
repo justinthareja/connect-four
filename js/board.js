637dccba8cf666adc425cf09c50ac647207e1726
Justin Thareja <justin.thareja@gmail.com>
2021-01-19 20:46:12 -0500

    added js to render board

--- js/board.js
+++ js/board.js
@@ -5,12 +5,39 @@
     function init() {
         $board = document.querySelector("[rel=js-board]");
         $board.addEventListener("click", handleBoardClick);
+
+        render();
     }
 
     function handleBoardClick(event) {
-        console.log("board clicked!");
+        console.log("hi");
     }
-    
+
+    // Takes a board array and returns the corresponding HTML string
+    function template(board) {
+        var html = "";
+
+        board.forEach(function renderRow(row) {
+            html += `<div class="board-row">`;
+            
+            row.forEach(function renderCell(cell) {
+                let classNames = "board-cell"
+                if (cell == 1) classNames += " red";
+                if (cell == 2) classNames += " yellow";
+
+                html += `<div class="${classNames}" rel="js-cell"></div>`;
+            });
+
+            html += `</div>`;
+        });
+
+        return html;
+    }
+
+    function render() {
+        $board.innerHTML = template(connect4.board);
+    }
+
     EVT.on("init", init);
     
     var publicAPI = {};
